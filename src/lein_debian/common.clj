8444c32a6ce80292d6d8e6e4c09114cc722df261
Jan Krueger <jan.krueger@gateprotect.com>
2012-05-14 09:57:31 +0200

    Only append / when not already present

--- src/lein_debian/common.clj
+++ src/lein_debian/common.clj
@@ -46,7 +46,12 @@
 
 (defn path
   [element & more-elements]
-  (str/join "/" (conj more-elements element)))
+  (reduce (fn [path s]
+            (if (.endsWith path "/")
+              (str path s)
+              (str path "/" s)))
+          element
+          more-elements))
 
 (defn build-debian-version
   [version build-num]
